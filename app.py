201b16aee07f969e6a51ccf458a4123160538579
Moris Wu <36097270+MorisWu@users.noreply.github.com>
2023-04-20 10:08:26 +0000

    linebotGPT

--- app.py
+++ app.py
@@ -1,42 +1,71 @@
+import os
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
 from flask import Flask, request, abort
+import openai
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+LINE_CHANNEL_ACCESS_TOKEN = 'Your LINE CHANNEL ACCESS TOKEN'
+LINE_CHANNEL_SECRET = 'Your LINE CHANNEL SECRET'
+OPENAI_API_KEY = 'Your OpenAI API key'
+
+# 初始化Line Bot API
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# 初始化OpenAI API
+openai.api_key = OPENAI_API_KEY
+
+user_dict = {}
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('giz4GEgHUsXPauBPJBWB1LzyxUwO8d+HyDr4zASK7vBe2/LAElviZ+3IIFc5M2WOSVsdtX/3g1uE6vkr7f/pbMM/Iyk33RMBcPpfB05qGSfEDL2wCQEpwwJq7jWFhuZobxwLT6g8pyk+kumwhlBIsgdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('7485e6889f5ff6e7a769f74d347dc2ad')
-
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 確認請求是否來自 Line 平臺
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-# 處理訊息
+# 處理訊息事件
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
 
-import os
+    if not event.source.user_id in user_dict:
+        print('add: ' + event.source.user_id)
+        user_dict[event.source.user_id] = {}
+        user_dict[event.source.user_id]['message'] = []
+        user_displayname = line_bot_api.get_profile(event.source.user_id).display_name
+        user_dict[event.source.user_id]['display_name'] = user_displayname
+
+
+    user_dict[event.source.user_id]['message'].append({"role": "user", "content": "盡量簡短但不要太簡短的回應以下句子："+event.message.text})
+
+    # 使用OpenAI API產生回覆訊息
+    response = openai.ChatCompletion.create(model= "gpt-3.5-turbo",
+                                            messages=user_dict[event.source.user_id]['message'],
+                                            n=1,
+                                            max_tokens=300,
+                                            stop=None,
+                                            temperature=0.5)
+
+    # 使用Line Bot API回復使用者的訊息
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=response['choices'][0]['message']['content'])
+    )
+    user_dict[event.source.user_id]['message'].append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+
+    if len(user_dict[event.source.user_id]['message']) > 8:
+        for i in range(2):
+            user_dict[event.source.user_id]['message'].remove(user_dict[event.source.user_id]['message'][i])
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 50))
+    app.run(host="0.0.0.0", port=port)
